f28abfe7bd8db02701ea299f4ebcee43382ddd4f
jtstriker3 <jtstriker3@gmail.com>
2014-09-09 15:33:37 -0400

    Changing to assembly version to 2.0.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Joe.Web.Mvc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Joe Corporation")]
+[assembly: AssemblyCompany("Joe Dev")]
 [assembly: AssemblyProduct("Joe.Web.Mvc")]
-[assembly: AssemblyCopyright("Copyright © Joe Corporation 2012")]
+[assembly: AssemblyCopyright("Copyright © Joe Dev 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
